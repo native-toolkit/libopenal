01961754dc212f523ff5f64c126d1fd6e3f080be
Joshua Granick <jgranick@users.noreply.github.com>
2016-09-10 09:39:43 -0700

    Initial work on Mac config (going to switch to OpenAL.framework instead)

--- include/config.h
+++ include/config.h
@@ -1,39 +1,6 @@
 #ifdef HX_MACOS
 
-	/* Define to the library version */
-	#define ALSOFT_VERSION "1.17.2"
-
-	// Why does cmake need this?
-	#define SIZEOF_LONG sizeof(long)
-	#define SIZEOF_LONG_LONG sizeof(long long)
-
-	/* API declaration export attribute */
-	#define AL_API  __attribute__((visibility("default")))
-	#define ALC_API __attribute__((visibility("default")))
-
-	/* Define any available alignment declaration */
-	#define ALIGN(x) __attribute__((aligned(x)))
-
-	/* Define to the appropriate 'restrict' keyword */
-	#define RESTRICT restrict
-	#define HAVE_POSIX_MEMALIGN
-	#define HAVE_SSE
-	#define HAVE_COREAUDIO
-	#define HAVE_WAVE
-	#define HAVE_STAT
-	#define HAVE_LRINTF
-	#define HAVE_STRTOF
-	#define HAVE_GCC_DESTRUCTOR
-	#define HAVE_GCC_FORMAT
-	#define HAVE_STDINT_H
-	#define HAVE_DLFCN_H
-	#define HAVE_XMMINTRIN_H
-	#define HAVE_CPUID_H
-	#define HAVE_FLOAT_H
-	#define HAVE_FENV_H
-	#define HAVE_FESETROUND
-	#define HAVE_PTHREAD_SETSCHEDPARAM
-
+#include "config-macos-x86_64.h"
 
 #elif defined(HX_WINDOWS)
 
